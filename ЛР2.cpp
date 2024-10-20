6f745ad9f19bbd83d9d7ab2086ab109ec09e5e96
Baranov Sergiy <onlykomzypc@gmail.com>
2024-10-20 19:13:26 +0300

    локалізовано кирилицю

--- ЛР2.cpp
+++ ЛР2.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 int main()
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	cout << "String1\n" << '\t' << "String2\n" << '\t' << '\t' << "String3\n";
 	cout << '\t' << "String1\n" << "String2\n";
 	cout << "3" << "*" << '\t' << "String\n1\n";
