d126a710eec04e2deade8f18bf7b19aa8d08bae6
Komzyyy <onlykomzypc5633@gmail.com>
2024-10-20 18:56:50 +0300

    Завантажено лабораторну роботу з ОП

---
+++ ЛР2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+	cout << "String1\n" << '\t' << "String2\n" << '\t' << '\t' << "String3\n";
+	cout << '\t' << "String1\n" << "String2\n";
+	cout << "3" << "*" << '\t' << "String\n1\n";
+}
